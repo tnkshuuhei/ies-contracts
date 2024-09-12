c7235307bf61ff8375fa5115e2874c935eca2dc2
shuhei tanaka <shuhei46491@gmail.com>
2024-09-12 16:29:34 +0900

    chore: add errors

---
+++ src/libraries/Errors.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity >=0.8.25;
+
+/// @title Errors
+/// @notice Library containing all custom errors the protocol may revert with.
+contract Errors {
+    /// ======================
+    /// ====== Generic =======
+    /// ======================
+
+    /// @notice Thrown as a general error when input / data is invalid
+    error INVALID();
+
+    /// @notice Thrown when mismatch in decoding data
+    error MISMATCH();
+
+    /// @notice Thrown when not enough funds are available
+    error NOT_ENOUGH_FUNDS();
+
+    /// @notice Thrown when user is not authorized
+    error UNAUTHORIZED();
+
+    /// @notice Thrown when address is the zero address
+    error ZERO_ADDRESS();
+
+    /// @notice Thrown when the function is not implemented
+    error NOT_IMPLEMENTED();
+
+    /// @notice Thrown when the value is non-zero
+    error NON_ZERO_VALUE();
+}
